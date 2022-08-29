e9eea9d0f7aa14831ee76f13c6daf0dc55b055b9
Hlib ArcherAV Holubov <66315154+ArcherV1nce@users.noreply.github.com>
2022-08-29 19:21:43 +0300

    Changed Healthbar logic
    Changed Healthbar logic from calculations of the slider using Update to Coroutine or DoTween.

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,16 +1,23 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 [RequireComponent(typeof(Slider))]
 public class HealthBar : MonoBehaviour
 {
     private const float AnimationDurationMin = 1f;
     private const float AnimationDurationMax = 60f;
+    private const int AnimationStepsPerSecond = 50;
 
     [SerializeField] private Character _character;
     [SerializeField] [Range(AnimationDurationMin, AnimationDurationMax)] 
     private float _animationDuration;
+    [SerializeField] private bool _useDoTween;
 
+    private Coroutine _healthbarUpdate;
+    private float _lastSliderValue;
+    private float _animationStepsCount;
     private Slider _slider;
 
     private void Awake()
@@ -20,8 +27,7 @@ public class HealthBar : MonoBehaviour
 
     private void OnValidate()
     {
-        _animationDuration = _animationDuration < AnimationDurationMin ? AnimationDurationMin : _animationDuration;
-        _animationDuration = _animationDuration > AnimationDurationMax ? AnimationDurationMax : _animationDuration;
+        ValidateAnimation();
     }
 
     private void Update()
@@ -29,16 +35,66 @@ public class HealthBar : MonoBehaviour
         ShowCurrentHealth();
     }
 
+    private bool CheckHealthChanges(float healthToSliderValue)
+    {
+        bool healthChanged = false;
+
+        if (_lastSliderValue != healthToSliderValue)
+        {
+            _lastSliderValue = healthToSliderValue;
+            healthChanged = true;
+        }
+
+        return healthChanged;
+    }
+
+    private IEnumerator AnimateHealthChange(float duration)
+    {
+        var waitForNextStep = new WaitForSecondsRealtime(duration / _animationStepsCount);
+        float stepDistance = Mathf.Abs(_slider.value - _lastSliderValue) / _animationStepsCount;
+
+        while (_slider.value != _lastSliderValue)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value,
+                _lastSliderValue, stepDistance);
+            yield return waitForNextStep;
+        }
+    }
+
     private void ShowCurrentHealth ()
     {
-        _slider.value = Mathf.MoveTowards(_slider.value, 
-            _character.Health/_character.HealthFull, 
-            Time.deltaTime / _animationDuration);
+        float healthToSliderValue = _character.Health / _character.HealthFull;
+
+        if (CheckHealthChanges(healthToSliderValue))
+        {
+            if (_healthbarUpdate != null)
+            {
+                StopCoroutine(_healthbarUpdate);
+            }
+
+            if (_useDoTween)
+            {
+                _slider.DOValue(healthToSliderValue, _animationDuration);
+            }
+
+            else
+            {
+                _healthbarUpdate = StartCoroutine(AnimateHealthChange(_animationDuration));    
+            }
+        }   
     }
 
     private void SetUp ()
     {
         _slider = GetComponent<Slider>();
         _slider.value = _character.Health / _character.HealthFull;
+        _animationStepsCount = AnimationStepsPerSecond * _animationDuration;
+    }
+
+    private void ValidateAnimation()
+    {
+        _animationDuration = _animationDuration < AnimationDurationMin ? AnimationDurationMin : _animationDuration;
+        _animationDuration = _animationDuration > AnimationDurationMax ? AnimationDurationMax : _animationDuration;
+        _animationStepsCount = AnimationStepsPerSecond * _animationDuration;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Healthbar task
  Simple health visualization
+ 
+ Packages used in this project:
+ DoTween: http://dotween.demigiant.com/license.php
